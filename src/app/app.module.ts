7f6791b6cbd12736fa3bb5df632cecdaf524bdb7
blazejszum <muzsbl@gmail.com>
2018-11-01 19:07:06 +0100

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FilterComponent } from './filter/filter.component';
+import { BookListComponent } from './book-list/book-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FilterComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
